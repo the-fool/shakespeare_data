5f5c9bc006107d6b2fd4b48263ae2770731db65b
tr <sketchbang@gmail.com>
2018-11-11 11:23:23 -0600

    add json

---
+++ make_json.py
@@ -0,0 +1,19 @@
+import csv
+import json
+
+tables = [
+    'characters',
+    'paragraphs',
+    'scenes',
+    'works'
+]
+
+d = {}
+
+for table in tables:
+    with open(f'csv/{table}.csv', 'r') as in_file:
+        reader = csv.DictReader(in_file)
+        d[table] = list(reader)
+
+with open('json/normalized.json', 'w') as out_file:
+    out_file.write(json.dumps(d, sort_keys=True, indent=2))
